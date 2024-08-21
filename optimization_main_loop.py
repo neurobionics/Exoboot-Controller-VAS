a309fb45e13d88db8384ef9df1c5168f86d33496
Nundini <nundinir@umich.edu>
2024-08-21 00:02:40 -0400

    repo reset for VAS protocol

--- optimization_main_loop.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-pardir = os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__))) + '/preference_learning_dephy_exo/online_optimization'
-print(pardir)
-sys.path.append(pardir)
-import main_loop
