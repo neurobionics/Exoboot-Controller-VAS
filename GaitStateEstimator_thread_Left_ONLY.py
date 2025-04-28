5d38f1932eff08434c12dfe7500b62bb70372c42
Nundini Rawal <nundinir@umich.edu>
2025-04-28 14:54:20 +0100

    flashed new SD card with 32bit rpi4 OS. Added 2 environments - python 3.9 and python 3.11 - to test which is compatible with flexsea v8.0.1 lib. Success with py3.9, rpi 4, 32bit OS combo with flexsea 8.0.1 lib.

---
+++ GaitStateEstimator_thread_Left_ONLY.py
@@ -0,0 +1,166 @@
+# Description: 
+# This file contains a class which calculates gait phase based on the average of recent stride durations.
+#
+# Author: Varun Satyadev Shetty
+# Date: 06/17/2024
+# Sensor reading logic modified based on exoboot structure by Max Shepherd
+import time, copy, threading
+from typing import Type
+
+#from rtplot import client
+from Reference_Scripts_Bertec_Sync.ZMQ_PubSub import Subscriber
+from BaseExoThread import BaseThread
+from utils import MovingAverageFilter
+from GroundContact import BertecEstimator
+from SoftRTloop import FlexibleSleeper
+
+from constants import VICON_IP, GSETHREAD_FIELDS
+
+class GaitStateEstimator(BaseThread):
+    """
+    Description
+    """
+    def __init__(self, startstamp, device_left, thread_left, name='GSE', filter_size=10, daemon=True, continuousmode=False, quit_event=Type[threading.Event], pause_event=Type[threading.Event], log_event=Type[threading.Event]):
+        super().__init__(name, daemon, quit_event, pause_event, log_event)
+        self.device_left = device_left
+        self.device_thread_left = thread_left
+
+        # Filter size
+        self.filter_size = filter_size
+
+        # Operating mode
+        self.continuousmode = continuousmode
+
+        # Peak torques set by GUI
+        self.peak_torque_left = 0
+        self.peak_torque_right = 0
+
+        # Logging fields
+        self.fields = GSETHREAD_FIELDS
+        self.data_dict = dict.fromkeys(self.fields)
+
+        # LoggingNexus
+        self.startstamp = startstamp
+        self.loggingnexus = None
+
+        # Link to devices
+        self.link_to_device()
+
+    def link_to_device(self):
+        self.device_left.gse = self
+
+    def set_peak_torque_left(self, T):
+        self.peak_torque_left = T
+        if self.continuousmode:
+            self.device_thread_left.peak_torque = self.peak_torque_left
+
+    def set_peak_torque_right(self, T):
+        pass
+
+    def get_sensor_data(self):
+        """TODO implement"""
+        pass
+
+    def get_estimate(self):
+        """TODO implement"""
+        pass
+    
+    def on_pre_run(self):
+        """
+        Runs once before starting main loop
+        """
+        # Bertec subscribers and estimators
+        self.sub_bertec_right = Subscriber(publisher_ip=VICON_IP,topic_filter='fz_right',timeout_ms=5)
+        self.sub_bertec_left = Subscriber(publisher_ip=VICON_IP,topic_filter='fz_left',timeout_ms=5)
+        self.bertec_estimator = BertecEstimator(self.sub_bertec_left, self.sub_bertec_right, filter_size=self.filter_size)
+
+        # RealTimePlotting of: left & right angle angle, actual ankle torque, ankle velocity, and commanded torque
+        # client.configure_ip(RTPLOT_IP)
+        # # plot_1_config = {'names': ['Ankle Angle Left'], 'title': "Ankle Angle Left", 'colors': ['r'], 'yrange':[20, 130], 'ylabel': "degrees", 'xlabel': 'timestep', "line_width":[8,8]}
+        # # plot_5_1_config = {'names': ['Desired Torque Left'], 'title': "Desired Torque Left", 'colors': ['b'], 'yrange':[0,40], 'ylabel': "Nm", 'xlabel': 'timestep',"line_width":[8,8]}
+        # # plot_5_1_1_config = {'names': ['Calcd Torque Left'], 'title': "Calcd Torque Left", 'colors': ['r'], 'yrange':[0, 40], 'ylabel': "Nm", 'xlabel': 'timestep',"line_width":[8,8]}
+        # # plot_9_config = {'names': ['In Swing Left'], 'title': "Swing Left", 'colors': ['r'], 'yrange':[0, 100], 'ylabel': "degrees", 'xlabel': 'timestep',"line_width":[8,8]}
+        # # plot_10_config = {'names': ['Accel Y Left'], 'title': "Accel Y Left", 'colors': ['r'], 'yrange':[-10, 50], 'ylabel': "degrees", 'xlabel': 'timestep',"line_width":[8,8]}
+ 
+        # # all_plot_configs = [plot_1_config, plot_2_config,plot_3_config, plot_3_1_config, plot_4_config, plot_5_config, plot_6_config]
+        # all_plot_configs = [plot_1_config, plot_5_1_config, plot_5_1_1_config, plot_9_config, plot_10_config]
+        # client.initialize_plots(all_plot_configs)
+        
+        # Period Tracker
+        self.period_tracker = MovingAverageFilter(size=500)
+        self.prev_end_time = time.perf_counter()
+
+        # Soft real time loop
+        loopFreq = 500 # Hz
+        loop_period = 1 / loopFreq
+        self.softRTloop = FlexibleSleeper(period=loop_period)
+
+    def pre_iterate(self, pause_event):
+        """
+        Sensor and Bertec reading
+        Runs even if threads are paused
+        """
+        # Set starting time stamp
+        self.data_dict['pitime'] = time.perf_counter() - self.startstamp
+
+        # TODO IMU Estimation
+        # self.get_sensor_data()
+        # self.get_estimate()
+
+        new_stride_flag_left, new_stride_flag_right, force_left, force_right = self.bertec_estimator.get_estimate(pause_event)
+
+        # Add forces to data dict
+        self.data_dict['forceplate_left'] = force_left
+        self.data_dict['forceplate_right'] = force_right
+
+        return new_stride_flag_left, new_stride_flag_right
+
+    def iterate(self, new_stride_flag_left, new_stride_flag_right):
+        """
+        Update device estimates
+        Does not run when threads paused
+        """
+        # Update exoboot threads if new state estimate
+        if new_stride_flag_left:
+            HS_l, stride_period_l, in_swing_l = self.bertec_estimator.return_estimate_left()
+            self.device_thread_left.set_state_estimate(HS_l, stride_period_l, self.peak_torque_left, in_swing_l)
+
+        
+    def post_iterate(self):
+        """
+        Loop period tracking and soft real time pause
+        """
+        # Update Period Tracker
+        end_time = time.perf_counter()
+        self.period_tracker.update(end_time - self.prev_end_time)
+        self.prev_end_time = end_time
+        my_freq = 1/self.period_tracker.average()
+
+        # Log gse freq
+        self.data_dict['thread_freq'] = my_freq
+        if self.loggingnexus and self.log_event.is_set():
+            self.loggingnexus.append(self.name, copy.deepcopy(self.data_dict))
+
+        # soft real-time loop
+        self.softRTloop.pause()
+
+    def run(self):
+        """
+        Custom run to continue catching heelstrike but not update estimates
+        """
+        self.on_pre_run()
+        try:
+            # print("GSE QUIT: ", self.quit_event.is_set())
+            while self.quit_event.is_set():
+                # print("GSE RUNNING")
+                nslf, nsfr = self.pre_iterate(self.pause_event)
+                if self.pause_event.is_set():
+                    self.iterate(nslf, nsfr)
+                else:
+                    pass
+                # print("GSE EXIT")
+                self.post_iterate()
+        except Exception as e:
+            print("ERROR {}: {}".format(self.name, e))
+        finally:
+            pass
