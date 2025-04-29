017380b8315e08f64658b9f563315e915cd4c492
Nundini Rawal <nundinir@umich.edu>
2025-04-29 20:19:11 +0100

    initial changes

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# Import specific sub-modules for easy access in controller_main.py
+from .src.settings.constants import *
+from .src.settings.config import *
+from .src.exo.AssistanceGenerator_new import *
--- controller_main.py
+++ controller_main.py
@@ -15,22 +15,85 @@ from flexsea import flexsea as flex
 from flexsea import fxUtils as fxu
 from flexsea import fxEnums as fxe
 
-def get_active_ports(fxs):
-    """
-    Scans and returns a list of active ports corresponding to each exoskeleton 
-    """
-    
-    
-    
-    return flex.get_active_ports()
+from opensourceleg.utilities import SoftRealtimeLoop
+from opensourceleg.logging.logger import Logger
+from opensourceleg.actuators.dephy import DEFAULT_POSITION_GAINS, DephyActuator
+
+from .src.settings.constants import BAUD_RATE, DEV_ID_TO_SIDE_DICT
+
+class MainController:
+    def __init__(self):
+        self.fxs = flex.FlexSEA()     # instantiate the flexsea library
     
 
+    def get_active_ports(self):
+        """
+        Opens known active ports (ttyACM0 & ttyACM1) and establishes a connection 
+        with devices. Also matches device ID to a side (left/right).
+        
+        Atleast 1 device has to be connected. Only connected devices will be controlled. 
+        
+        arguments:
+        - None
+        
+        returns:
+        - dev_id: device ID of the exoskeleton
+        - side: side of the exoskeleton (left/right)
+        """
+        try:
+            # obtain device ID
+            dev_id_1 = self.fxs.open(port="/dev/ttyACM0", baud_rate=BAUD_RATE, log_level=3)
+            
+            # determine which side the device_id corresponds to
+            side_1 = DEV_ID_TO_SIDE_DICT[dev_id_1]
+            
+            print("Device 1: {}, {}".format(dev_id_1, side_1))
+        except:
+            side_1 = None
+            dev_id_1 = None
+            print("DEVICE 1 NOT FOUND")
+            
+        try:
+            # obtain device ID
+            dev_id_2 = self.fxs.open(port="/dev/ttyACM1", baud_rate=BAUD_RATE, log_level=3)
+            
+            # determine which side the device_id corresponds to
+            side_2 = DEV_ID_TO_SIDE_DICT[dev_id_2]
+            
+            print("Device 1: {}, {}".format(dev_id_2, side_2))
+        except:
+            side_2 = None
+            dev_id_2 = None
+            print("DEVICE 2 NOT FOUND")
+       
+
+        # if neither exoboots are connected, exit the program
+        if not (dev_id_1 or dev_id_2):
+            print("\nNO DEVICES: CONNECT AND POWER ON ATLEAST 1 EXOBOOT\n")
+            quit()
+
+        # always assign first pair of outputs to left side
+        if side_1 == "left" or side_2 == "right":
+            return side_1, dev_id_1, side_2, dev_id_2
+        elif side_1 == "right" or side_2 == "left":
+            return side_2, dev_id_1, side_1, dev_id_2
+        else:
+            raise Exception("Invalid sides for devices: Check DEV_ID_TO_SIDE_DICT!")
+    
+    def run(self):
+        side_1, dev_id_1, side_2, dev_id_2 = self.get_active_ports()    # open & connect to devices
+        
+        # TODO: instantiate each exoskeleton object
+        
+        # TODO: start streaming device, set gains
+        
+        # TODO:spool belts
+        
+        # iterate though FSM Controller
+        
+        # exit gracefully either via keyboard interrupt or by exiting the program
+        
 
 if __name__ == '__main__':
+    MainController.run()
     
-    # initialize the flexsea library
-    fxs = flex.FlexSEA()
-  
-    # get active ports
-    active_ports = get_active_ports(fxs)
-    print(f"Active ports: {active_ports}")
