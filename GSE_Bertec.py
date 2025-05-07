8152e3030e42d193bbbcac1336a763c441a10e27
htchnsn <htchnsn@umich.edu>
2025-05-07 00:00:12 -0400

    Rewrote BertecEstimator in GroundContact as Bertec_Estimator.py in GSE_Bertec.py. Key differences are in the init, must specify a single zmq subscriber. Now one Bertec_Estimator per side of the treadmill. update() changed accordingly. Added additional comments to clarify sections of code. Removed stance period tracking (unused). Last key difference is the omission of ACCEPT_STRIDE_THRESHOLD, which previously rejected any newly observed stride period outside of 20% of the pperiod tracker's average.

---
+++ GSE_Bertec.py
@@ -0,0 +1,82 @@
+import time
+
+from src.utils.utils import MovingAverageFilter
+
+class Bertec_Estimator:
+    def __init__(self, zmq_subscriber, stride_period_init=1.2, filter_size=10, HS_THRESHOLD = 50, TO_THRESHOLD = 20):
+        
+        # ZMQ subscriber
+        self.subsciber = zmq_subscriber
+
+        # Constants
+        self.HS_THRESHOLD = HS_THRESHOLD
+        self.TO_threshold = TO_THRESHOLD
+
+        # State variables
+        self.HS = 0
+        self.TO = 0
+        self.force_prev = 0
+        self.contact = False    # True == in stance
+
+        self.stride_period_tracker = MovingAverageFilter(initial_value=stride_period_init, filter_size=filter_size)
+
+    def return_estimate(self):
+        """
+        Return most recent state of estimator (a, b, c)
+            a) most recent heel strike time
+            b) average stride period
+            c) in swing
+            
+        """
+        
+        return self.HS, self.stride_period_tracker.average(), not self.contact
+
+    def update(self):
+        """
+        Update estimator state with new force data
+        Returns (a, b)
+            a) if new stride has been observed
+            b) force from Bertec
+        """
+
+        # ZMQ streaming get message
+        topic, force, timestep_valid = self.subscriber.get_message()
+
+        # Catch empty messages
+        force = self.force_prev if force == '' else float(force)
+
+        # New stride flag
+        new_stride = False
+
+        # Determine state
+        if self.in_contact:
+            if force < self.TO_THRESHOLD:
+                # New Toe off
+                self.in_contact = False
+                self.TO = time.time()
+
+            else:
+                # In stance
+                self.in_contact = True
+
+        else:
+            if force >= self.HS_THRESHOLD:
+                # New Heel strike
+                self.in_contact = True
+                new_stride = True
+
+                # Record new stride period and update estimate
+                HS_new = time.time()
+                stride_period_new = HS_new - self.HS
+                self.stride_period_tracker.update(stride_period_new)
+
+                self.HS = HS_new
+
+            else:
+                # In swing
+                self.in_contact = False
+
+        # Update prev
+        self.force_prev = force
+
+        return new_stride, force
