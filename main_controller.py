6334fde37150ed0231c8acc442da6061869e4506
Nundini Rawal <nundinir@umich.edu>
2025-05-28 17:22:21 +0100

    began initial refactor of JIM characterization script

--- main_controller.py
+++ main_controller.py
@@ -1,20 +1,20 @@
 """
-Description: 
+Description:
 
 This script is the main controller for the Dephy EB-51 Exoskeletons.
 It is responsible for initializing the exoskeletons and running the main control loop.
 
-NOTE: 
+NOTE:
 This script deliberately uses the downgraded 8.0.1 flexsea library.
-The newer flexsea libraries have an integer overflow issue, preventing 
+The newer flexsea libraries have an integer overflow issue, preventing
 new sensor data from being received. This issue only exists for the exoskeleton devices
-(not the actpacks). The 8.0.1 library is stable with a Raspberry Pi 4B with a 32-bit OS, 
+(not the actpacks). The 8.0.1 library is stable with a Raspberry Pi 4B with a 32-bit OS,
 using python 3.9.
 
 Date: 04/29/2025
 Author(s): Nundini Rawal
 """
-from rtplot import client 
+from rtplot import client
 
 from opensourceleg.logging import Logger, LogLevel
 from opensourceleg.utilities import SoftRealtimeLoop
@@ -42,18 +42,18 @@ if __name__ == '__main__':
     actuators = create_actuators(1, BAUD_RATE, FLEXSEA_FREQ, LOG_LEVEL)
 
     exoboots = DephyExoboots(
-        tag="exoboots", 
-        actuators=actuators, 
+        tag="exoboots",
+        actuators=actuators,
         sensors={}
     )
-    
+
     # TODO: instantiate an assistance generator
-    
+
     # TODO: instantiate FSM for exoboots ~ swing, stance, passive
 
     # set-up the soft real-time loop:
-    clock = SoftRealtimeLoop(dt = 1/250) 
-    
+    clock = SoftRealtimeLoop(dt = 1/250)
+
     # set-up logging:
     logger = Logger(log_path=log_path,
                     file_name=file_name+"_incline",
@@ -62,62 +62,62 @@ if __name__ == '__main__':
                     stream_level = LogLevel.INFO
                     )
     exoboots.track_variables_for_logging(logger)
-    
+
     # add in GSE's
     sub_bertec_left = Subscriber(publisher_ip=VICON_IP,topic_filter='fz_left',timeout_ms=5)
     bertec_estimator = Bertec_Estimator(zmq_subscriber=sub_bertec_left)
     imu_estimator = IMU_Estimator()
-    
+
     logger.track_variable(lambda: int(not bertec_estimator.in_contact), f"in_swing")
     logger.track_variable(lambda: int(imu_estimator.activation_state), f"activation_state")
     logger.track_variable(lambda: bertec_estimator.force_prev, f"forceplate")
-    
+
     # set-up real-time plots:
     client.configure_ip(RTPLOT_IP)
     plot_config = exoboots.initialize_rt_plots()
     client.initialize_plots(plot_config)
-    
+
     with exoboots:
-        
+
         exoboots.setup_control_modes()
-        
+
         # spool belts upon startup
         # exoboots.spool_belts()
-            
+
         for _t in clock:
             try:
-                
+
                 # update robot sensor states
                 exoboots.update()
-                
+
                 # TODO: determine current gait state
-                
+
                 # TODO: determine appropriate torque setpoint using assistance generator
-                
+
                 # TODO: determine appropriate current setpoint that matches the torque setpoint (updates transmission ratio internally)
                 # currents = exoboots.find_current_setpoints(ankle_torque_setpt)
-                
+
                 # TODO: command appropriate current setpoint (internally ensures that current in mA is a integer)
                 # exoboots.command_currents(currents)
-                
+
                 # TODO: receive any NEW grpc values/inputs for next iteration
 
                 # update GSE's
                 imu_estimator.update(exoboots.right.accelx)
                 bertec_estimator.update()
-                
+
                 # TODO: Add control logic here
-                
+
                 # update real-time plots & send data to server
                 data_to_plt = exoboots.update_rt_plots(not bertec_estimator.in_contact, imu_estimator.activation_state)
                 client.send_array(data_to_plt)
-                
+
             except KeyboardInterrupt:
                 print("Keyboard interrupt detected. Exiting...")
                 logger.flush_buffer()
                 logger.close()
                 break
-            
+
             except Exception as err:
                 print("Unexpected error in executing main controller:", err)
                 logger.close()
